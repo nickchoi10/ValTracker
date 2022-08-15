acf88e32b1438b88dbba6779f1a5a1d0524e7c47
Nick Choi <96934316+nickchoi10@users.noreply.github.com>
2022-08-15 11:35:44 -0400

    springbootapp

---
+++ src/main/java/com/valorant/valtracker/ValtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.valorant.valtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValtrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valorant/valtracker/ValtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valorant.valtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
