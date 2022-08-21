b34d8f7d6ad55ebc2292b4ee329878d032afa512
Nick Choi <96934316+nickchoi10@users.noreply.github.com>
2022-08-21 19:46:17 -0400

    fetch elo

--- display/src/components/Profile.jsx
+++ display/src/components/Profile.jsx
@@ -1,10 +1,13 @@
 import { React, useContext} from 'react';
 import {Heading,Flex,Text,Image,useColorModeValue,SkeletonCircle,Skeleton} from '@chakra-ui/react';
 import { UserContext } from '../lib/UserContext';
+import { MdEditLocation, MdMeetingRoom, MdRMobiledata } from 'react-icons/md';
 
 export default function Profile() {
   const bg = useColorModeValue('#FFFFFF', '#121212');
   const { data } = useContext(UserContext);
+  const { mmr } = useContext(UserContext);
+
 
   return (
     <Flex
@@ -14,42 +17,34 @@ export default function Profile() {
       flexDirection={{ md: 'column', base: 'row' }}
       justifyContent="center">
         
-      <Flex flexDirection={{ md: 'column', base: 'column' }} alignItems="center"> 
-        <Text color="#C0C2C8" fontWeight="semibold">
-          Profile
-        </Text>
-
+        <Flex flexDirection={{ md: 'column', base: 'column' }} alignItems="center"> 
+            <Text color="#C0C2C8" fontWeight="semibold">
+                Profile
+            </Text>
 
             <Flex flexDirection="column">
                 {data.card ? ( <Image src={data.card.small}/>) : (
-              <SkeletonCircle/>
-            )}
-            {data.name ? (
-              <Heading size={{ md: 'md' }}  >
-                {data.name} #{data.tag}
-              </Heading>
-            ) : (
-              <Skeleton mt="20px" height="30px">
-              </Skeleton>)
-              }
-            </Flex>
-
-            
-            <Flex>
-                <Text fontWeight="bold" color="#46B8A3">
-                  Level:
-                </Text>
-                {data.account_level ? (
-                  <Text fontWeight="bold">
-                    {data.account_level}
-                  </Text>
-                ) : (
-                  <Skeleton>
-                  </Skeleton>
+                <SkeletonCircle/>
                 )}
+                {data.name ? (
+                    <Heading size={{ md: 'md' }}  >
+                    {data.name} #{data.tag}
+                    </Heading>
+                ) : (
+                <Skeleton mt="20px" height="30px">
+                </Skeleton>)
+                }
             </Flex>
 
-      </Flex>
+                <Text fontWeight="bold" color="#46B8A3">
+                    Level:  {data.account_level}
+                </Text>
+
+                <Text fontWeight="bold" color="#46B8A3">
+                    Elo:  {mmr.elo}
+                </Text>
+
+        </Flex>
     </Flex>
   );
 }
